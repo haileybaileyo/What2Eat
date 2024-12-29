09d5661d56cef65346cf4655b57bcb33672f145d
suyeun0109 <102908528+suyeun0109@users.noreply.github.com>
2024-12-29 22:41:29 +0900

    기본설정

---
+++ FirstProj/src/main/java/com/example/FirstProj/FirstProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjApplication.class, args);
+	}
+
+}
